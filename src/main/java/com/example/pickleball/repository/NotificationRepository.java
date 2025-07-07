b11eec4a838e3086fe4973355f0368a951ef5fb8
Chân Hòa <hoanc5197@ut.edu.vn>
2025-07-07 15:40:36 +0700

    Update NotificationRepository.java

--- src/main/java/com/example/pickleball/repository/NotificationRepository.java
+++ src/main/java/com/example/pickleball/repository/NotificationRepository.java
@@ -3,7 +3,6 @@ package com.example.pickleball.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.pickleball.model.Notification;
